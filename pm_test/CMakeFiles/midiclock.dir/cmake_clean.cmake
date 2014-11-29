a1e47b0b602561fc875b5d3c38cd2ff85d73b148
jm-discord <jasonmanning23@gmail.com>
2014-11-30 04:21:05 +0800

    Patched to work on OSX10.10 and Linux3.16

---
+++ pm_test/CMakeFiles/midiclock.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/midiclock.dir/midiclock.c.o"
+  "../midiclock.pdb"
+  "../midiclock"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/midiclock.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ pm_test/CMakeFiles/sysex.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sysex.dir/sysex.c.o"
+  "../sysex.pdb"
+  "../sysex"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/sysex.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
